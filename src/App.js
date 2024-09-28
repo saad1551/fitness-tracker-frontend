84b35c1e3b47c4bb18fd28c941e0844c5d33d4b3
saad1551 <saad.ashraf.9094@gmail.com>
2024-09-28 18:38:13 +0500

    add complete registraion and login

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Register from './Pages/Register';
 import Home from './Pages/Home';
 import VerifyEmail from './Components/VerifyEmail';
 import CompleteRegistration from './Pages/CompleteRegistration';
+import Login from './Pages/Login';
+import axios from 'axios';
+
+// Set default Axios config globally
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
@@ -14,6 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/verifyEmail/:verificationToken" element={<VerifyEmail />} />
           <Route path="/completeregistraion" element={<CompleteRegistration />} />
         </Routes>
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -11,17 +11,16 @@ import { useNavigate } from 'react-router-dom';
 
 
 const initialState = {
-    phone: '',
-    age: '',
-    workout_time: ''
+    email: '',
+    password: ''
 };
 
-const CompleteRegistration = () => {
+const Login = () => {
     const [formData, setFormData] = useState(initialState);
 
     const [isLoading, setIsLoading] = useState(false);
 
-    const navigate = useNavigate('/');
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -39,12 +38,12 @@ const CompleteRegistration = () => {
 
         try {
             setIsLoading(true);
-            const response = await axios.post(`${backendUrl}/api/users/completeregistration`, formData);
+            const response = await axios.post(`${backendUrl}/api/users/login`, formData);
             setIsLoading(false);
             if (response.status === 200) {
                 toast.success(response.data.message);
-                navigate('/');
             }
+            navigate('/');
         } catch (error) {
             setIsLoading(false);
             toast.error(error.response.data.message);
@@ -59,37 +58,28 @@ const CompleteRegistration = () => {
         <Logo />
         {isLoading && <Loader />}
         <Form onSubmit={(e) => handleSubmit(e)}>
-            <Form.Group as={Row} className="mb-3" controlId="formHorizontalName">
-                <Form.Label column sm={3}>
-                Phone
-                </Form.Label>
-                <Col sm={9}>
-                <Form.Control maxLength={12} minLength={12} type="text" placeholder="XXXX-XXXXXXX" name="phone" value={formData.phone} onChange={(e) => handleChange(e)} />
-                </Col>
-            </Form.Group>
-
             <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
                 <Form.Label column sm={3}>
-                Age
+                Email
                 </Form.Label>
                 <Col sm={9}>
-                <Form.Control type="number" placeholder="Age" name="age" value={formData.age} onChange={(e) => handleChange(e)} />
+                <Form.Control type="email" placeholder="Email" name="email" value={formData.email} onChange={(e) => handleChange(e)} />
                 </Col>
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
                 <Form.Label column sm={3}>
-                Workout Time
+                Password
                 </Form.Label>
                 <Col sm={9}>
-                <Form.Control type="time" placeholder="Workout Time" name="workout_time" value={formData.workout_time} onChange={(e) => handleChange(e)} />
+                <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={(e) => handleChange(e)} />
                 </Col>
             </Form.Group>
 
 
             <Form.Group as={Row} className="mb-3">
                 <Col sm={{ span: 10, offset: 2 }}>
-                <Button disabled={isLoading} type="submit">Complete Registraion</Button>
+                <Button disabled={isLoading} type="submit">Login</Button>
                 </Col>
             </Form.Group>
         </Form>
@@ -97,4 +87,4 @@ const CompleteRegistration = () => {
   )
 }
 
-export default CompleteRegistration;
+export default Login;
