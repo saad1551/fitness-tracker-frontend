207afb60a9f34ad274bc6685dda8eba7516fece4
saad1551 <saad.ashraf.9094@gmail.com>
2024-09-29 01:43:58 +0500

    add starter code for dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
-const Dashboard = () => {
+function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <Tabs
+      defaultActiveKey="profile"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="home" title="Home">
+        Tab content for Home
+      </Tab>
+      <Tab eventKey="workout_history" title="Workout History">
+        Tab content for Workout History
+      </Tab>
+      <Tab eventKey="progress_charts" title="Progress Charts">
+        Tab content for Progress Charts
+      </Tab>
+      <Tab eventKey="profile" title="Profile">
+        Tab content for Profile
+      </Tab>
+    </Tabs>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
