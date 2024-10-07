b45c9b3d4221d09af11cc1b211a8f8d285a2a2d8
saad1551 <saad.ashraf.9094@gmail.com>
2024-10-07 12:55:58 +0500

    make exercises page responsive by reducing pagination buttons

--- src/Pages/Exercises.js
+++ src/Pages/Exercises.js
@@ -23,6 +23,21 @@ const Exercises = ({ modalClose, resetTimer, pauseTimer }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const exercisesPerPage = 10;
 
+    // For responsive pagination
+    const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     useEffect(() => {
         const getBodyParts = async () => {
             const url = "https://exercisedb.p.rapidapi.com/exercises/bodyPartList";
@@ -86,7 +101,7 @@ const Exercises = ({ modalClose, resetTimer, pauseTimer }) => {
         textAlign: "center",
         borderRadius: "8px",
         boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)"
-    }
+    };
 
     // Pagination logic
     const indexOfLastExercise = currentPage * exercisesPerPage;
@@ -129,9 +144,10 @@ const Exercises = ({ modalClose, resetTimer, pauseTimer }) => {
 
     // Function to create pagination window
     const getPaginationWindow = () => {
-        const windowSize = 5;
-        const start = Math.max(1, currentPage - Math.floor(windowSize / 2));
-        const end = Math.min(totalPages, start + windowSize - 1);
+        // Show 3 pages on mobile, otherwise 5
+        const windowSizeForPagination = windowSize < 768 ? 3 : 5;
+        const start = Math.max(1, currentPage - Math.floor(windowSizeForPagination / 2));
+        const end = Math.min(totalPages, start + windowSizeForPagination - 1);
 
         let pages = [];
         for (let i = start; i <= end; i++) {
@@ -141,97 +157,86 @@ const Exercises = ({ modalClose, resetTimer, pauseTimer }) => {
     };
 
     return (
-            <div className="exercises-container">
-                {showModal && <ExerciseModal resetTimer={resetTimer} pauseTimer={pauseTimer} modalClose={modalClose} exercise={exercise} handleClose={() => setShowModal(false)} />}
-        
-                {/* Search and Total Exercises Container */}
-                <div className="search-card-container">
-                    <div className="exercises-search-container">
-                        <input
-                            type="text"
-                            placeholder="Search exercises..."
-                            value={search}
-                            onChange={(e) => setSearch(e.target.value)}
-                            className="exercises-search-input"
-                        />
-        
-                        <DropdownButton
-                            id="dropdown-basic-button"
-                            title={bodyPart.toUpperCase()}
-                            variant="outline-primary"
-                            className="body-part-dropdown"
-                            drop="end"
-                        >
-                            {bodyPartList.map((part, index) => (
-                                <Dropdown.Item key={index} onClick={() => handleBodyPartSelect(part)}>
-                                    {part.toUpperCase()}
-                                </Dropdown.Item>
-                            ))}
-                        </DropdownButton>
-                    </div>
-        
-                    {/* Total Exercises Card
-                    <Card style={cardStyles}>
-                        <Card.Body style={{padding: "20px"}}>
-                            <Card.Title style={{  fontSize: "1.5rem", fontWeight: "bold"}}>Total Exercises</Card.Title>
-                            <Card.Text style={{ fontSize: "1.2rem" }}>{exerciseData.length}</Card.Text>
-                        </Card.Body>
-                    </Card> */}
+        <div className="exercises-container">
+            {showModal && <ExerciseModal resetTimer={resetTimer} pauseTimer={pauseTimer} modalClose={modalClose} exercise={exercise} handleClose={() => setShowModal(false)} />}
 
-                    <div className={search !== '' ? 'cards' : ''}>
-                        <div style={{ ...cardStyles, marginRight: "10px" }}>
-                            <div style={{ padding: "20px" }}>
-                                <h2 style={{ fontSize: "1.5rem", fontWeight: "bold" }}>Total Exercises</h2>
-                                <p style={{ fontSize: "1.2rem" }}>{exerciseData.length}</p>
-                            </div>
-                        </div>
-                        {search !== '' &&           
-                        <div style={{ ...cardStyles, marginLeft: "10px" }}>
-                            <div style={{ padding: "20px" }}>
-                                <h2 style={{ fontSize: "1.5rem", fontWeight: "bold" }}>Filtered Exercises</h2>
-                                <p style={{ fontSize: "1.2rem" }}>{filteredData.length}</p>
-                            </div>
-                        </div>}
+            {/* Search and Total Exercises Container */}
+            <div className="search-card-container">
+                <div className="exercises-search-container">
+                    <input
+                        type="text"
+                        placeholder="Search exercises..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="exercises-search-input"
+                    />
 
-                    </div>
-
-
+                    <DropdownButton
+                        id="dropdown-basic-button"
+                        title={bodyPart.toUpperCase()}
+                        variant="outline-primary"
+                        className="body-part-dropdown"
+                        drop="end"
+                    >
+                        {bodyPartList.map((part, index) => (
+                            <Dropdown.Item key={index} onClick={() => handleBodyPartSelect(part)}>
+                                {part.toUpperCase()}
+                            </Dropdown.Item>
+                        ))}
+                    </DropdownButton>
                 </div>
-        
-                {isLoading && <Loader />}
-        
-                {!isLoading && currentExercises.map((exercise) => (
-                    <ExerciseRow key={exercise.id} exercise={exercise} onStart={(exercise) => handleRowClick(exercise)} />
-                ))}
-        
-                {/* Pagination Controls */}
-                <div className="pagination">
-                    <button onClick={goToFirstPage} disabled={currentPage === 1}>
-                        First
-                    </button>
-                    <button onClick={goToPreviousPage} disabled={currentPage === 1}>
-                        Previous
-                    </button>
-        
-                    {getPaginationWindow().map((page) => (
-                        <button
-                            key={page}
-                            onClick={() => paginate(page)}
-                            className={page === currentPage ? 'active-page' : ''}
-                        >
-                            {page}
-                        </button>
-                    ))}
-        
-                    <button onClick={goToNextPage} disabled={currentPage === totalPages}>
-                        Next
-                    </button>
-                    <button onClick={goToLastPage} disabled={currentPage === totalPages}>
-                        Last
-                    </button>
+
+                <div className={search !== '' ? 'cards' : ''}>
+                    <div style={{ ...cardStyles, marginRight: "10px" }}>
+                        <div style={{ padding: "20px" }}>
+                            <h2 style={{ fontSize: "1.5rem", fontWeight: "bold" }}>Total Exercises</h2>
+                            <p style={{ fontSize: "1.2rem" }}>{exerciseData.length}</p>
+                        </div>
+                    </div>
+                    {search !== '' &&           
+                    <div style={{ ...cardStyles, marginLeft: "10px" }}>
+                        <div style={{ padding: "20px" }}>
+                            <h2 style={{ fontSize: "1.5rem", fontWeight: "bold" }}>Filtered Exercises</h2>
+                            <p style={{ fontSize: "1.2rem" }}>{filteredData.length}</p>
+                        </div>
+                    </div>}
                 </div>
             </div>
-        );
+
+            {isLoading && <Loader />}
+
+            {!isLoading && currentExercises.map((exercise) => (
+                <ExerciseRow key={exercise.id} exercise={exercise} onStart={(exercise) => handleRowClick(exercise)} />
+            ))}
+
+            {/* Pagination Controls */}
+            <div className="pagination">
+                <button onClick={goToFirstPage} disabled={currentPage === 1}>
+                    {windowSize < 768 ? '<<' : 'First'}
+                </button>
+                <button onClick={goToPreviousPage} disabled={currentPage === 1}>
+                    {windowSize < 768 ? '<' : 'Previous'}
+                </button>
+
+                {getPaginationWindow().map((page) => (
+                    <button
+                        key={page}
+                        onClick={() => paginate(page)}
+                        className={page === currentPage ? 'active-page' : ''}
+                    >
+                        {page}
+                    </button>
+                ))}
+
+                <button onClick={goToNextPage} disabled={currentPage === totalPages}>
+                    {windowSize < 768 ? '>' : 'Next'}
+                </button>
+                <button onClick={goToLastPage} disabled={currentPage === totalPages}>
+                    {windowSize < 768 ? '>>' : 'Last'}
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Exercises;
